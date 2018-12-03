4b8a2fd1f3bed99c3d936e2166cff23207d90bc5
mbehboodi <mbehboodi6909@gmail.com>
2018-12-03 10:44:11 +0330

    add webpack development configuration

---
+++ webpack.dev.js
@@ -0,0 +1,136 @@
+'use strict';
+
+const path = require('path');
+
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'app.js'),
+    context: path.resolve(__dirname),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: path.join('bundle.[chunkhash].[contentHash].js'),
+        hashDigestLength: 32,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                include: [
+                    path.resolve(__dirname, 'src')
+                ],
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            hmr: true,
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            camelCase: true,
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceComments: true,
+                            sourceMap: true,
+                            sourceMapContents: true,
+                            outputStyle: 'expanded',
+                            includePaths: [
+                                './node_modules'
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'assets/fonts'
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.(html)$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            attrs: [':data-image-src', 'img:src', 'a:href']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|svg)/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'assets/images'
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.(pug)/,
+                use: [
+                    {
+                        loader: 'pug-loader',
+                        options: {
+                            pretty: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        modules: [
+            'node_modules'
+        ]
+    },
+    devtool: 'source-map',
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.pug'),
+            filename: path.join('index.html')
+        })
+    ],
+    devServer: {
+        contentBase: './dist',
+        port: 8080,
+        hot: true
+    }
+};
