ecb73e623984aaa3ffd654bd1177ff46b83f0f07
mbehboodi <mbehboodi6909@gmail.com>
2018-12-13 14:05:42 +0330

    add vue webpack import alias

--- webpack.common.js
+++ webpack.common.js
@@ -4,21 +4,24 @@ const path = require('path');
 
 module.exports = {
     entry: {
+        // about: path.resolve(__dirname, 'src', 'pages', 'about', 'index.js'),
         home: path.resolve(__dirname, 'src', 'pages', 'home', 'index.js'),
-        form: path.resolve(__dirname, 'src', 'pages', 'form', 'index.js'),
-        result: path.resolve(__dirname, 'src', 'pages', 'result', 'index.js'),
-        about: path.resolve(__dirname, 'src', 'pages', 'about', 'index.js')
+        // form: path.resolve(__dirname, 'src', 'pages', 'form', 'index.js'),
+        // result: path.resolve(__dirname, 'src', 'pages', 'result', 'index.js')
     },
     context: path.resolve(__dirname),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: path.join('[name]', 'bundle.[hash].js'),
+        filename: path.join('[name]', 'bundle.[contentHash].js'),
         hashDigestLength: 32,
     },
     resolve: {
         modules: [
             'node_modules'
-        ]
+        ],
+        alias: {
+            vue: 'vue/dist/vue'
+        }
     },
     plugins: []
 };
