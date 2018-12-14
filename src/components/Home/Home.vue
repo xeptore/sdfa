2084405f6ef990aa71f2f1e71eea6325e9823285
mbehboodi <mbehboodi6909@gmail.com>
2018-12-14 07:03:44 +0330

    move Home page component to components directory

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+include home
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<style lang="scss">
+</style>
+
+
+<style lang="scss" scoped>
+    @import '../_shared/styles/main.scss';
+    @import './styles/style.scss';
+</style>
--- src/pages/home/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import Home from './Home.vue';
-
-const vm = new Vue({
-    el: '#app',
-    components: {Home},
-    template: '<Home/>'
-});
