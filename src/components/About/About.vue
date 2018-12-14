191eaa34661dde324c7c52e0d3a5cf8d37c4b094
mbehboodi <mbehboodi6909@gmail.com>
2018-12-14 07:02:20 +0330

    move About component to components directory

---
+++ src/components/About/About.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+#about.container
+    include about
+</template>
+
+<script>
+
+export default {
+    name: 'About'
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../_shared/styles/main.scss';
+    @import './styles/style.scss';
+</style>
