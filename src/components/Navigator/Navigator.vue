0d8cff81a837eed2ecf4d60062eb268fee2a154c
mbehboodi <mbehboodi6909@gmail.com>
2018-12-14 07:03:10 +0330

    move Navigator component to components directory

--- src/components/Navigator/Navigator.vue
+++ src/components/Navigator/Navigator.vue
@@ -13,7 +13,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    @import '../../styles/main.scss';
+    @import '../_shared/styles/main.scss';
     @import './navigator.scss';
 </style>
 
