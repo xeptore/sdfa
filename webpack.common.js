fcf500f948cc3b8ddaa65959e5d9c88d8a339b21
mbehboodi <mbehboodi6909@gmail.com>
2018-12-14 07:00:07 +0330

    replace all webpack entry points with single main

--- webpack.common.js
+++ webpack.common.js
@@ -4,10 +4,7 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        about: path.resolve(__dirname, 'src', 'pages', 'about', 'index.js'),
-        home: path.resolve(__dirname, 'src', 'pages', 'home', 'index.js'),
-        form: path.resolve(__dirname, 'src', 'pages', 'form', 'index.js'),
-        result: path.resolve(__dirname, 'src', 'pages', 'result', 'index.js')
+        main: path.resolve(__dirname, 'src', 'app.js')
     },
     context: path.resolve(__dirname),
     output: {
