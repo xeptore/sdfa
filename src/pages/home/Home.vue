63229f4d0752102f265f9096d61436433cd71758
mbehboodi <mbehboodi6909@gmail.com>
2018-12-14 06:06:56 +0330

    replace unnecessary template sections from home.pug with navigator vue component

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,11 +1,24 @@
 <template lang="pug">
-#home
-    include home
+div
+    .container-fluid.d-flex.flex-column.align-items-center
+        div(dir="rtl").col-10.wrapper.d-flex.flex-column.align-items-center.justify-content-around
+            include home
+
+    <Breadcrumb active="home"/>
+
+    <Navigator/>
 </template>
 
 <script>
-export default {
+import Breadcrumb from '../../shared/templates/breadcrumb/Breadcrumb.vue';
+import Navigator from '../../shared/templates/navigator/Navigator.vue';
 
+export default {
+    name: 'Home',
+    components: {
+        Breadcrumb,
+        Navigator
+    }
 }
 </script>
 
