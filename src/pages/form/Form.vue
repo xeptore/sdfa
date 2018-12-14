acd97d5a8a17443815bc96982a410a4c672d4dc1
mbehboodi <mbehboodi6909@gmail.com>
2018-12-14 06:06:43 +0330

    replace unnecessary template sections from form.pug with navigator and breadcrumb vue component

--- src/pages/form/Form.vue
+++ src/pages/form/Form.vue
@@ -1,11 +1,24 @@
 <template lang="pug">
-#form
-    include form
+div
+    .container-fluid.d-flex.flex-column.align-items-center
+        div(dir="rtl").col-10.wrapper.d-flex.flex-column.align-items-center.justify-content-around
+            include form
+
+    <Breadcrumb active="form"/>
+
+    <Navigator/>
 </template>
 
 <script>
-export default {
+import Breadcrumb from '../../shared/templates/breadcrumb/Breadcrumb.vue';
+import Navigator from '../../shared/templates/navigator/Navigator.vue';
 
+export default {
+    name: 'Form',
+    components: {
+        Breadcrumb,
+        Navigator
+    }
 }
 </script>
 
