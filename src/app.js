026a585afd1d290ca0529546b6f72be396650839
mbehboodi <mbehboodi6909@gmail.com>
2018-12-14 21:32:24 +0330

    add App component as the primary component

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ import './components/_shared/styles/main.scss'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import { Routes } from './routes/router'
+import App from './App.vue'
 
 Vue.use(VueRouter)
 
 // eslint-disable-next-line no-unused-vars
 const app = new Vue({
   router: Routes(),
-  el: '#app'
+  el: '#app',
+  components: { App },
+  template: '<App/>'
 })
