3b9e695059b8f003f2b95e06f8c055d47482fbc8
mbehboodi <mbehboodi6909@gmail.com>
2018-12-14 07:03:27 +0330

    move Form page component to components directory

---
+++ src/components/Form/Form.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+include form
+</template>
+
+<script>
+export default {
+    name: 'Form'
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../_shared/styles/main.scss';
+    @import './styles/style.scss';
+</style>
--- src/pages/form/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import Form from './Form.vue';
-
-const vm = new Vue({
-    el: '#app',
-    components: {Form},
-    template: '<Form/>'
-});
