39414ddec319b6493621a861118edd7f6253c3aa
mbehboodi <mbehboodi6909@gmail.com>
2018-12-14 06:07:06 +0330

    replace unnecessary template sections from result.pug with navigator vue component

--- src/pages/result/Result.vue
+++ src/pages/result/Result.vue
@@ -1,11 +1,24 @@
 <template lang="pug">
-#result
-    include result
+div
+    .container-fluid.d-flex.flex-column.align-items-center
+        div(dir="rtl").col-10.wrapper.d-flex.flex-column.align-items-center.justify-content-around
+            include result
+
+    <Breadcrumb active="result"/>
+
+    <Navigator/>
 </template>
 
 <script>
-export default {
+import Breadcrumb from '../../shared/templates/breadcrumb/Breadcrumb.vue';
+import Navigator from '../../shared/templates/navigator/Navigator.vue';
 
+export default {
+    name: 'Result',
+    components: {
+        Breadcrumb,
+        Navigator
+    }
 }
 </script>
 
