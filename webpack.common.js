a4523b63cc4aa3c8fb7e8c011c5383149c22539c
mbehboodi <mbehboodi6909@gmail.com>
2018-12-15 08:09:43 +0330

    code style

--- webpack.common.js
+++ webpack.common.js
@@ -1,24 +1,24 @@
-'use strict';
+'use strict'
 
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, 'src', 'app.js')
-    },
-    context: path.resolve(__dirname),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: path.join('bundle.[contentHash].js'),
-        hashDigestLength: 32,
-    },
-    resolve: {
-        modules: [
-            'node_modules'
-        ],
-        alias: {
-            vue: 'vue/dist/vue'
-        }
-    },
-    plugins: []
-};
+  entry: {
+    main: path.resolve(__dirname, 'src', 'app.js')
+  },
+  context: path.resolve(__dirname),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: path.join('bundle.[contentHash].js'),
+    hashDigestLength: 32
+  },
+  resolve: {
+    modules: [
+      'node_modules'
+    ],
+    alias: {
+      vue: 'vue/dist/vue'
+    }
+  },
+  plugins: []
+}
