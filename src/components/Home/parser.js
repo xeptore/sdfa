a525abcceb33003114aca7537169d980cc2efcee
mbehboodi <mbehboodi6909@gmail.com>
2018-12-16 10:59:33 +0330

    fix parser DFA class Accepts method Map.get() key-value existance fallback

--- src/components/Home/parser.js
+++ src/components/Home/parser.js
@@ -29,6 +29,9 @@ export class DFA {
     this.Accepts = input => {
       let state = this.Initial
       for (const ch of input) {
+        if (!this.Transitions.has(state) || !this.Transitions.get(state).has(ch)) {
+          return false
+        }
         state = this.Transitions.get(state).get(ch)
       }
       return typeof state !== 'undefined' && this.Acceptings.map(es => String(es)).includes(state)
