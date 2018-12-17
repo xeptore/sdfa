34496a12da47a880785e8dc9c214f3490ea9575a
mbehboodi <mbehboodi6909@gmail.com>
2018-12-17 22:07:39 +0330

    use Globals ValidationResult property in result page component data

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -2,11 +2,18 @@ import Wrapper from '../Wrapper/Wrapper.vue'
 import Breadcrumb from '../Breadcrumb/Breadcrumb.vue'
 import Navigator from '../Navigator/Navigator.vue'
 
+import Globals from '../../globals'
+
 export default {
   name: 'Result',
   components: {
     Wrapper,
     Breadcrumb,
     Navigator
+  },
+  data () {
+    return {
+      result: Globals.ValidationResult
+    }
   }
 }
