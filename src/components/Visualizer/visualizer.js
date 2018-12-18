9005188afd2726f2372ab637f8c023bd3105ec18
mbehboodi <mbehboodi6909@gmail.com>
2018-12-18 10:30:22 +0330

    complete dfa visualization with viz

--- src/components/Visualizer/visualizer.js
+++ src/components/Visualizer/visualizer.js
@@ -1,3 +1,5 @@
+import Wrapper from '../Wrapper/Wrapper.vue'
+
 import StateMachine from 'javascript-state-machine'
 import Visualize from 'javascript-state-machine/lib/visualize'
 
@@ -8,23 +10,27 @@ const { Module, render } = require('viz.js/full.render')
 
 export default {
   name: 'Visualizer',
-  created () {
-    const fsmConfig = { init: '0', transitions: [] }
+  components: {
+    Wrapper
+  },
+  mounted () {
+    const fsmConfig = { init: 0, transitions: [] }
     for (const [pk, pv] of Globals.DFA.Transitions.entries()) {
       for (const [ck, cv] of pv.entries()) {
         fsmConfig.transitions.push({ name: ck, from: pk, to: cv })
       }
     }
 
-    console.log('DFA:', Globals.DFA)
-    console.log('fsm:', fsmConfig)
-
     const fsm = new StateMachine(fsmConfig)
-    const dot = Visualize(fsm)
+    const dot = Visualize(fsm, { name: 'DFA', orientation: 'horizontal' })
+
     const viz = new Viz({ Module, render })
     viz.renderSVGElement(dot).then(res => {
-      console.log('res:', res)
-      document.getElementById('diagram').appendChild(res)
+      const wrapper = document.getElementById('diagram')
+      wrapper.appendChild(res)
+      const svg = wrapper.getElementsByTagName('svg')[0]
+      svg.setAttribute('height', '100%')
+      svg.setAttribute('width', '100%')
     })
   }
 }
