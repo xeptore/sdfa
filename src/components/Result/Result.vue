b9072b7d7e1aa0fff8f3054931d783c59178708c
mbehboodi <mbehboodi6909@gmail.com>
2018-12-19 07:57:09 +0330

    import spinkit and please-wait styles

--- src/components/Result/Result.vue
+++ src/components/Result/Result.vue
@@ -10,7 +10,9 @@ div
 
 <script src="./Result.js"></script>
 
-<style lang="scss" scoped>
-    @import '../_shared/styles/main.scss';
-    @import './styles/style.scss';
+<style lang="scss">
+@import "../_shared/styles/main.scss";
+@import "~please-wait/src/please-wait.scss";
+@import "~spinkit/scss/spinners/2-double-bounce.scss";
+@import "./styles/style.scss";
 </style>
