3901fb0ca3aa15a70920024c0d8e88f696b9a116
mbehboodi <mbehboodi6909@gmail.com>
2018-12-20 17:07:41 +0330

    fix webpack production configuration - use webpack HashedModuleIdsPlugin plugin - use [hash] webpack substitutions in output file names - use optimization chunking - import chunks in webpack-generated template - add css modules rule - code style

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const path = require('path')
 
+const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
@@ -32,86 +33,127 @@ const minificationConfig = {
 module.exports = merge(common, {
   mode: 'production',
   output: {
-    filename: path.join('app.js')
+    filename: path.join('[name].js')
   },
   module: {
-    rules: [{
-      test: /\.vue$/,
-      loader: 'vue-loader'
-    },
-    {
-      test: /\.js$/,
-      include: path.resolve(__dirname, 'src'),
-      use: 'babel-loader'
-    },
-    {
-      test: /\.scss$/,
-      include: [
-        path.resolve(__dirname, 'src')
-      ],
-      use: [{
-        loader: MiniCssExtractPlugin.loader,
-        options: {}
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
       },
       {
-        loader: 'css-loader',
-        options: {}
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        use: 'babel-loader'
       },
       {
-        loader: 'postcss-loader',
-        options: {}
+        test: /\.scss$/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {}
+          },
+          {
+            loader: 'css-loader',
+            options: {}
+          },
+          {
+            loader: 'postcss-loader',
+            options: {}
+          },
+          {
+            loader: 'resolve-url-loader',
+            options: {
+              debug: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              outputStyle: 'compressed',
+              includePaths: [
+                './node_modules'
+              ]
+            }
+          }
+        ]
       },
       {
-        loader: 'resolve-url-loader',
-        options: {}
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {}
+          },
+          {
+            loader: 'css-loader',
+            options: {}
+          },
+          {
+            loader: 'postcss-loader',
+            options: {}
+          },
+          {
+            loader: 'resolve-url-loader',
+            options: {
+              debug: true
+            }
+          }
+        ]
       },
       {
-        loader: 'sass-loader',
+        test: /\.(woff|woff2|eot|ttf)$/,
+        loader: 'file-loader',
         options: {
-          sourceMap: true,
-          outputStyle: 'compressed',
-          includePaths: [
-            './node_modules'
-          ]
+          name: '[name].[ext]',
+          publicPath: 'assets/fonts',
+          outputPath: 'assets/fonts'
         }
-      }
-      ]
-    },
-    {
-      test: /\.(woff|woff2|eot|ttf)$/,
-      loader: 'file-loader',
-      options: {
-        name: '[name].[ext]',
-        publicPath: 'assets/fonts',
-        outputPath: 'assets/fonts'
-      }
-    },
-    {
-      test: /\.(jpe?g|png|svg)/,
-      use: [{
+      },
+      {
+        test: /\.(jpe?g|png|svg)/,
         loader: 'file-loader',
         options: {
           name: '[name].[ext]',
           publicPath: 'assets/images',
           outputPath: 'assets/images'
         }
-      } ]
-    },
-    {
-      test: /\.pug$/,
-      loader: 'pug-plain-loader'
-
-    }
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-plain-loader'
+      }
     ]
   },
+  optimization: {
+    runtimeChunk: 'single',
+    minimize: false,
+    splitChunks: {
+      name: true,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  },
   plugins: [
-    new WebpackVisualizerPlugin(),
+    new webpack.HashedModuleIdsPlugin(),
+    new WebpackVisualizerPlugin({
+      filename: path.join('stats', 'visualize.html')
+    }),
     new BundleAnalyzerPlugin({
       analyzerMode: 'static',
       openAnalyzer: false,
       generateStatsFile: true,
-      statsFilename: path.join('analyzer', 'stats.json'),
-      reportFilename: path.join('analyzer', 'report.html')
+      statsFilename: path.join('stats', 'stats.json'),
+      reportFilename: path.join('stats', 'report.html')
     }),
     new UglifyJsPlugin({
       extractComments: /^\**|@preserve|@liscence|@cc_on/i,
@@ -122,14 +164,14 @@ module.exports = merge(common, {
       verbose: true
     }),
     new MiniCssExtractPlugin({
-      filename: path.join('style.css'),
-      chunkFilename: '[id].css'
+      filename: '[name].style.css',
+      chunkFilename: '[name].[contenthash].[hash].[chunkhash].css'
     }),
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'),
       filename: path.join('index.html'),
-      chunks: ['main'],
+      chunks: ['main', 'vendors', 'runtime'],
       meta: TemplateMeta
     })
   ],
