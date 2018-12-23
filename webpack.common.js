86a0bce65c6134b93af09718fceddd3f58506f7e
mbehboodi <mbehboodi6909@gmail.com>
2018-12-23 08:21:04 +0330

    add webpack config for electron main process

--- webpack.common.js
+++ webpack.common.js
@@ -2,23 +2,54 @@
 
 const path = require('path')
 
-module.exports = {
-  entry: {
-    main: path.resolve(__dirname, 'src', 'app.js')
+module.exports = [
+  {
+    entry: {
+      main: path.resolve(__dirname, 'src', 'app.js')
+    },
+    context: path.resolve(__dirname),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: path.join('bundle.[contentHash].js'),
+      hashDigestLength: 32
+    },
+    resolve: {
+      modules: [
+        'node_modules'
+      ],
+      alias: {
+        vue: 'vue/dist/vue'
+      }
+    },
+    plugins: []
   },
-  context: path.resolve(__dirname),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: path.join('bundle.[contentHash].js'),
-    hashDigestLength: 32
-  },
-  resolve: {
-    modules: [
-      'node_modules'
-    ],
-    alias: {
-      vue: 'vue/dist/vue'
+  {
+    entry: {
+      electron: path.resolve(__dirname, 'src', 'electron.js')
+    },
+    context: path.resolve(__dirname),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: path.join('electron.js')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: path.resolve(__dirname, 'src'),
+          use: 'babel-loader'
+        }
+      ]
+    },
+    resolve: {
+      modules: [
+        'node_modules'
+      ]
+    },
+    target: 'electron-main',
+    node: {
+      __dirname: false,
+      __filename: false
     }
-  },
-  plugins: []
-}
+  }
+]
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const _ = require('lodash')
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const webpack = require('webpack')
@@ -21,144 +22,151 @@ const TemplateMeta = {
   }
 }
 
-module.exports = merge(common, {
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.scss$/,
-        include: [
-          path.resolve(__dirname, 'src')
-        ],
-        use: [
+module.exports = _.values(merge(common,
+  [
+    {
+      mode: 'development',
+      module: {
+        rules: [
           {
-            loader: 'style-loader',
+            test: /\.vue$/,
+            loader: 'vue-loader'
+          },
+          {
+            test: /\.js$/,
+            include: path.resolve(__dirname, 'src'),
+            loader: 'babel-loader'
+          },
+          {
+            test: /\.scss$/,
+            include: [
+              path.resolve(__dirname, 'src')
+            ],
+            use: [
+              {
+                loader: 'style-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'css-loader',
+                options: {
+                  camelCase: true,
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'resolve-url-loader',
+                options: {
+                  sourceMap: true,
+                  keepQuery: true,
+                  debug: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceComments: true,
+                  sourceMap: true,
+                  sourceMapContents: true,
+                  outputStyle: 'expanded',
+                  includePaths: [
+                    './node_modules'
+                  ]
+                }
+              }
+            ]
+          },
+          {
+            test: /\.css$/,
+            use: [
+              {
+                loader: 'style-loader',
+                options: {}
+              },
+              {
+                loader: 'css-loader',
+                options: {}
+              },
+              {
+                loader: 'postcss-loader',
+                options: {}
+              },
+              {
+                loader: 'resolve-url-loader',
+                options: {
+                  debug: true
+                }
+              }
+            ]
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf)$/,
+            loader: 'file-loader',
             options: {
-              sourceMap: true
+              name: '[name].[ext]',
+              publicPath: '../assets/fonts',
+              outputPath: 'assets/fonts'
             }
           },
           {
-            loader: 'css-loader',
+            test: /\.(jpe?g|png|svg)/,
+            loader: 'file-loader',
             options: {
-              camelCase: true,
-              sourceMap: true
+              name: '[name].[ext]',
+              publicPath: '../assets/images',
+              outputPath: 'assets/images'
             }
           },
           {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true
-            }
+            test: /\.pug$/,
+            loader: 'pug-plain-loader'
           },
           {
-            loader: 'resolve-url-loader',
-            options: {
-              sourceMap: true,
-              keepQuery: true,
-              debug: true
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceComments: true,
-              sourceMap: true,
-              sourceMapContents: true,
-              outputStyle: 'expanded',
-              includePaths: [
-                './node_modules'
-              ]
+            test: /\.html$/,
+            include: [
+              path.join(__dirname, 'src', 'components', 'Visualizer')
+            ],
+            use: {
+              loader: 'html-loader'
             }
           }
         ]
       },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-            options: {}
-          },
-          {
-            loader: 'css-loader',
-            options: {}
-          },
-          {
-            loader: 'postcss-loader',
-            options: {}
-          },
-          {
-            loader: 'resolve-url-loader',
-            options: {
-              debug: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          publicPath: '../assets/fonts',
-          outputPath: 'assets/fonts'
+      devtool: 'source-map',
+      plugins: [
+        new CleanWebpackPlugin([path.resolve(__dirname, 'dist')]),
+        new webpack.HashedModuleIdsPlugin(),
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, 'src', 'index.html'),
+          filename: path.join('index.html'),
+          chunks: ['main', 'vendors', 'runtime'],
+          meta: TemplateMeta
+        }),
+        new BundleAnalyzerPlugin({
+          openAnalyzer: false
+        }),
+        new WriteFilePlugin()
+      ],
+      devServer: {
+        contentBase: './dist',
+        port: 8080,
+        historyApiFallback: {
+          index: 'index.html'
         }
       },
-      {
-        test: /\.(jpe?g|png|svg)/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          publicPath: '../assets/images',
-          outputPath: 'assets/images'
-        }
-      },
-      {
-        test: /\.pug$/,
-        loader: 'pug-plain-loader'
-      },
-      {
-        test: /\.html$/,
-        include: [
-          path.join(__dirname, 'src', 'components', 'Visualizer')
-        ],
-        use: {
-          loader: 'html-loader'
-        }
-      }
-    ]
-  },
-  devtool: 'source-map',
-  plugins: [
-    new CleanWebpackPlugin([path.resolve(__dirname, 'dist')]),
-    new webpack.HashedModuleIdsPlugin(),
-    new VueLoaderPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html'),
-      filename: path.join('index.html'),
-      chunks: ['main', 'vendors', 'runtime'],
-      meta: TemplateMeta
-    }),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false
-    }),
-    new WriteFilePlugin()
-  ],
-  devServer: {
-    contentBase: './dist',
-    port: 8080,
-    historyApiFallback: {
-      index: 'index.html'
+      target: 'electron-renderer'
+    },
+    {
+      mode: 'production'
     }
-  },
-  target: 'electron-renderer'
-})
+  ]
+))
