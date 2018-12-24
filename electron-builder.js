b6c0e6d45d9579c6ea598ecd47e9f0bc425e6f23
mbehboodi <mbehboodi6909@gmail.com>
2018-12-24 07:05:36 +0330

    add ia32 win32 platform architecture electron-builder build script

--- electron-builder.js
+++ electron-builder.js
@@ -1,20 +1,35 @@
 'use strict'
 
+const colors = require('colors')
+const { safeLoad } = require('js-yaml')
+const { readFileSync } = require('fs')
+const { join } = require('path')
 const electronBuilder = require('electron-builder')
 const Platform = electronBuilder.Platform
+
+let configuration
+try {
+  configuration = safeLoad(readFileSync(join(__dirname, 'electron-builder.yml'), 'utf8'))
+} catch (error) {
+  console.error(colors.red('error parsing configuration file'))
+}
 
 electronBuilder.build({
   targets: Platform.WINDOWS.createTarget('nsis', [electronBuilder.Arch.x64]),
   config: {
-    icon: 'src/assets/images/icons/app/app.ico',
-    nsis: {
-      oneClick: false,
-      installerIcon: 'src/assets/images/icons/installer/installer.ico',
-      uninstallerIcon: 'src/assets/images/icons/uninstaller/uninstaller.ico',
-      perMachine: false,
-      allowToChangeInstallationDirectory: true
-    }
+    icon: configuration.win.icon,
+    nsis: configuration.nsis
   }
 }).then(result => {
-  console.log(result)
+  console.log(colors.green(`sucessfully built at: ${result[1]}`))
+})
+
+electronBuilder.build({
+  targets: Platform.WINDOWS.createTarget('nsis', [electronBuilder.Arch.ia32]),
+  config: {
+    icon: configuration.win.icon,
+    nsis: configuration.nsis
+  }
+}).then(result => {
+  console.log(colors.green(`sucessfully built at: ${result[1]}`))
 })
